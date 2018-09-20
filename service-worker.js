4fc2812ff5d5d917390a9423d34fe82f9c7a85a0
WeiboFans <KodiAddonUser@gmail.com>
2018-09-20 16:53:52 -0400

    update actvate event listener

--- service-worker.js
+++ service-worker.js
@@ -45,7 +45,7 @@ self.addEventListener('install', function(e) {
 });
 
 self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
+  console.log('[ServiceWorker] Activate fixed by haoran');
   e.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
